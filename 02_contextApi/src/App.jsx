0e9d109ae394c91483b48f026dbd183626e4e11e
srikar <srikar.code01@gmail.com>
2025-01-25 21:18:36 +0530

    refactor App component layout; improve structure and styling

--- 02_contextApi/src/App.jsx
+++ 02_contextApi/src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className='bg-green-500'>
-        hello world
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* theme button */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* card */}
+        </div>
       </div>
-      <div className='text-red-500'>
-        this is srikar
-      </div>
-      <div className='text-red-500'>
-        hello world again
-      </div>
-      <div>
-        hello world for the third time
-      </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
