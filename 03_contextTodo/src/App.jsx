71baf68bff845ab16be6dbe1fd5cf236e4784bfb
srikar <srikar.code01@gmail.com>
2025-01-26 18:51:39 +0530

    add TodoContext with initial state and methods; update App component layout

--- 03_contextTodo/src/App.jsx
+++ 03_contextTodo/src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { TodoProvider } from './contexts/TodoContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  
 
   return (
     <>
       <TodoProvider>
-        <div className='w-full min-h-screen bg-zinc-700'>
-          
-        </div>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
       </TodoProvider>
     </>
   )
